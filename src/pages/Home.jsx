41cb59009134c47a733b774856cac6843b0f017c
Tanvi Naik <21co67@aitdgoa.edu.in>
2024-07-19 11:53:17 +0530

    Dynamic search, use of api, promises, await async,fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+
 const Home = () => {
-  return <div>WELCOME TO HOME PAGW</div>;
+  const [search, setSearch] = useState('');
+
+  const onInput = ev => {
+    setSearch(ev.target.value);
+  };
+
+  const searchSubmit = async ev => {
+    ev.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${search}`
+    );
+    const body = await response.json();
+    console.log(body);
+  };
+
+  return (
+    <div>
+      <form onSubmit={searchSubmit}>
+        <input type="text" value={search} onChange={onInput} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
